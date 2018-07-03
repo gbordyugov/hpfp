056c64417fb8306c399b702dea659232be63d8ce
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 19:03:21 +0200

    Chapter 26 Applicative instance for EitherT

--- chapter26/EitherT.hs
+++ chapter26/EitherT.hs
@@ -2,6 +2,10 @@ data EitherT e m a = EitherT { runEitherT :: Either e (m a) }
 
 instance (Functor m) => Functor (EitherT e m) where
   fmap f (EitherT ema) = EitherT $ fmap (fmap f) ema
+
+instance (Applicative m) => Applicative (EitherT e m) where
+  pure a = EitherT $ pure $ pure a
+  (EitherT fa) <*> (EitherT a) = EitherT $ (fmap (<*>) fa) <*> a
 {-
 instance (Monad m) => Monad (EitherT e m) where
   return a = EitherT $ return $ return a
