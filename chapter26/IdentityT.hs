a065dc12d8d7395c790a0a66acbe0cf466fefbe4
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 10:31:34 +0200

    Chapter 25 relying on built-in typeclasses for IdentityT

--- chapter26/IdentityT.hs
+++ chapter26/IdentityT.hs
@@ -1,37 +1,16 @@
--- import Control.Monad
-import Prelude ((.), ($))
-
-{-
- - Typeclasses definitions
- -}
-class MyFunctor f where
-  fmap :: (a -> b) -> f a -> f b 
-
-class (MyFunctor f) => MyApplicative f where
-  pure :: a -> f a
-  (<*>) :: f (a -> b) -> f a -> f b
-
-class (MyApplicative m) => MyMonad m where
-  return :: a -> m a
-  (>>=) :: m a -> (a -> m b) -> m b
-
-
-
 data IdentityT m a = IdentityT { runIdentityT :: m a }
 
 
-{-
- - instances
- -}
-
-instance (MyFunctor m) => MyFunctor (IdentityT m) where
+instance (Functor m) => Functor (IdentityT m) where
   fmap f (IdentityT ma) = IdentityT $ fmap f ma
 
-instance (MyApplicative m) => MyApplicative (IdentityT m) where
+
+instance (Applicative m) => Applicative (IdentityT m) where
   pure a = IdentityT (pure a)
   (IdentityT f) <*> (IdentityT a) = IdentityT (f <*> a)
 
-instance (MyMonad m) => MyMonad (IdentityT m) where
+
+instance (Monad m) => Monad (IdentityT m) where
   return a = IdentityT (return a)
   (IdentityT ma) >>= f =
     IdentityT $ ma >>= (runIdentityT . f)
