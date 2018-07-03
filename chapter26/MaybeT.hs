8ad743f3ac60e0d4c1a9c1f642fd5a318582534d
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 11:13:22 +0200

    Chapter 26 trying to implement Applicative instance for MaybeT

--- chapter26/MaybeT.hs
+++ chapter26/MaybeT.hs
@@ -3,3 +3,8 @@ data MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
 
 instance (Functor m) => Functor (MaybeT m) where
   fmap f (MaybeT ma) = MaybeT $ fmap (fmap f) ma
+
+
+instance (Applicative m) => Applicative (MaybeT m) where
+  pure a = MaybeT $ pure $ pure a
+  (MaybeT fab) <*> (MaybeT a) = MaybeT $ undefined -- TODO
