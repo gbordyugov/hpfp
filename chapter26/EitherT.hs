06bacdc8b2a5edfd210166ed58275e597f2488fd
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 19:08:01 +0200

    Chapter 26 monad instances of EitherT

--- chapter26/EitherT.hs
+++ chapter26/EitherT.hs
@@ -1,13 +1,19 @@
-data EitherT e m a = EitherT { runEitherT :: Either e (m a) }
+data EitherT e m a = EitherT { runEitherT :: m (Either e a) }
+
 
 instance (Functor m) => Functor (EitherT e m) where
   fmap f (EitherT ema) = EitherT $ fmap (fmap f) ema
 
+
 instance (Applicative m) => Applicative (EitherT e m) where
   pure a = EitherT $ pure $ pure a
   (EitherT fa) <*> (EitherT a) = EitherT $ (fmap (<*>) fa) <*> a
-{-
+
+
 instance (Monad m) => Monad (EitherT e m) where
   return a = EitherT $ return $ return a
-  (EitherT ema) >>= f = undefined
--}
+  (EitherT ema) >>= f = EitherT $ do
+    y <- ema
+    case y of
+      Left e  -> return $ Left e
+      Right x -> runEitherT $ f x
