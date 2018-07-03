41cf17bd70d22ed09b2e2edb07ae0abb4e2355fc
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 14:36:49 +0200

    Chapter 25 composing appicatives from scratch

--- chapter25/Compose.hs
+++ chapter25/Compose.hs
@@ -7,5 +7,5 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure = Compose . (pure . pure)
-  (Compose fab) <*> (Compose a) = Compose $ hab <*> a where
-    hab  = (<*>) <$> fab
+  (Compose f) <*> (Compose a) = Compose $ h <*> a where
+    h = (<*>) <$> f
