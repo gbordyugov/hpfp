7564bb34c6e54e34c0a9bb8eccb5c2c55ad9c957
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 12:28:24 +0200

    still tinkering with composing Alternatives

--- chapter25/Compose.hs
+++ chapter25/Compose.hs
@@ -6,5 +6,6 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
 
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
-  pure = Compose . pure . pure
-  (Compose fab) <*> (Compose a) = Compose $ fmap undefined a
+  pure = Compose . (pure . pure)
+  (Compose fab) <*> (Compose a) = Compose $ hab <*> a where
+    hab  = undefined
