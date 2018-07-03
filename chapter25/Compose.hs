6969362e897b60b7bf3f37071adceea9db7f2571
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 14:32:40 +0200

    Chapter 25 composing Applicatives done

--- chapter25/Compose.hs
+++ chapter25/Compose.hs
@@ -8,4 +8,4 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure = Compose . (pure . pure)
   (Compose fab) <*> (Compose a) = Compose $ hab <*> a where
-    hab  = undefined
+    hab  = (<*>) <$> fab
