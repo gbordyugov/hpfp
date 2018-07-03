8b1c2b026dc451a486a2917186b50d7397d791bb
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 11:27:25 +0200

    Chapter 25 stuck at making Compose an instance of Applicative

--- chapter25/Compose.hs
+++ chapter25/Compose.hs
@@ -1,4 +1,10 @@
 data Compose f g a = Compose { runCompose :: f (g a) }
 
+
 instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap h (Compose fga) = Compose $ fmap (fmap h) fga
+
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure = Compose . pure . pure
+  (Compose fab) <*> (Compose a) = Compose $ fmap undefined a
