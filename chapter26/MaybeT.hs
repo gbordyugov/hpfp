88ae149575ff6595bd247518d07f456b875ec476
Grigory Bordyugov <gregbg@gmail.com>
2018-07-03 18:07:33 +0200

    Chapter 26 Monad instance for MaybeT

--- chapter26/MaybeT.hs
+++ chapter26/MaybeT.hs
@@ -8,3 +8,12 @@ instance (Functor m) => Functor (MaybeT m) where
 instance (Applicative m) => Applicative (MaybeT m) where
   pure a = MaybeT $ pure $ pure a
   (MaybeT fab) <*> (MaybeT a) = MaybeT $ (fmap (<*>) fab) <*> a
+
+
+instance (Monad m) => Monad (MaybeT m) where
+  return a = MaybeT $ return $ return a
+  (MaybeT ma) >>= f = MaybeT $ do
+    a <- ma
+    case a of
+      Nothing -> return Nothing
+      Just x  -> runMaybeT $ f x
