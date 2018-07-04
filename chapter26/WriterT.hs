155b4a6283843f0df41c9e9b403307fdf3903892
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 17:17:47 +0200

    Chapter 26 monad instance for WriterT

--- chapter26/WriterT.hs
+++ chapter26/WriterT.hs
@@ -10,3 +10,10 @@ instance (Applicative m, Monoid w) => Applicative (WriterT w m) where
   pure x = WriterT $ pure (x, mempty)
   (WriterT f) <*> (WriterT a) = WriterT $ (fmap h f) <*> a where
     h (f, fw) = \(a, aw) -> (f a, fw `mappend` aw)
+
+
+instance (Monad m, Monoid w) => Monad (WriterT w m) where
+  return = pure
+  (WriterT maw) >>= f = WriterT $ do
+    (a, w) <- maw
+    runWriterT $ f a
