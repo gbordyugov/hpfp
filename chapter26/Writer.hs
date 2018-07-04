147c9656179e4e5d192e8768df9fe9e268fe468d
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 11:42:16 +0200

    Chapter 26 monad instance for Writer

--- chapter26/Writer.hs
+++ chapter26/Writer.hs
@@ -9,3 +9,9 @@ instance (Monoid w) => Applicative (Writer w) where
   pure x = Writer $ (x, mempty)
   (Writer f) <*> (Writer a) =
     Writer $ ((fst f) (fst a), (snd f) `mappend` (snd a))
+
+
+instance (Monoid w) => Monad (Writer w) where
+  return = pure
+  (Writer (a, w)) >>= f = let (Writer (a', w')) = f a
+    in Writer $ (a', w `mappend` w')
