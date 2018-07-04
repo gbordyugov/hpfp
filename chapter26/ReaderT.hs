cb519c933ccb4a342cb1192d2547591abb0be93f
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 17:40:57 +0200

    Chapter 26 started implementing Applicative instance for ReaderT

--- chapter26/ReaderT.hs
+++ chapter26/ReaderT.hs
@@ -1,4 +1,10 @@
 data ReaderT r m a = ReaderT { runReaderT :: r -> m a }
 
+
 instance (Functor m) => Functor (ReaderT r m) where
   fmap f (ReaderT rma) = ReaderT $ (\r -> fmap f (rma r))
+
+
+instance (Applicative m) => Applicative (ReaderT r m) where
+  pure x = ReaderT $ \r -> pure x
+  (ReaderT rmfa) <*> (ReaderT a) = ReaderT $ undefined
