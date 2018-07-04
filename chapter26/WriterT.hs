6e7191e1b1da7803ed5d558ee51466340a44c9a7
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 17:08:15 +0200

    Chapter 26 another formulation for <*> for Applicative WriterT, I am still not sure how it works :~)

--- chapter26/WriterT.hs
+++ chapter26/WriterT.hs
@@ -9,4 +9,4 @@ instance (Functor m) => Functor (WriterT w m) where
 instance (Applicative m, Monoid w) => Applicative (WriterT w m) where
   pure x = WriterT $ pure (x, mempty)
   (WriterT f) <*> (WriterT a) = WriterT $ (fmap h f) <*> a where
-    h (f, fw) (a, aw) = (f a, fw `mappend` aw)
+    h (f, fw) = \(a, aw) -> (f a, fw `mappend` aw)
