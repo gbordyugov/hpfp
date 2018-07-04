cd2c4e7de7781eb24d15bccacf2753be85f0724e
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 11:50:10 +0200

    Chapter 26 WriterT functor instance

--- chapter26/WriterT.hs
+++ chapter26/WriterT.hs
@@ -1,1 +1,6 @@
 data WriterT w m a = WriterT { runWriterT :: m (a, w) }
+
+
+instance (Functor m) => Functor (WriterT w m) where
+  fmap f (WriterT maw) =
+    WriterT $ fmap (\(a, w) -> (f a, w)) maw
