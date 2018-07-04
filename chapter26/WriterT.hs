aaacd5725bedb61dfb7cfa761c42db509b7124df
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 11:58:08 +0200

    Chapter 26 postponed <*> for applicative instance for WriterT

--- chapter26/WriterT.hs
+++ chapter26/WriterT.hs
@@ -4,3 +4,9 @@ data WriterT w m a = WriterT { runWriterT :: m (a, w) }
 instance (Functor m) => Functor (WriterT w m) where
   fmap f (WriterT maw) =
     WriterT $ fmap (\(a, w) -> (f a, w)) maw
+
+
+instance (Applicative m, Monoid w) =>
+                          Applicative (WriterT w m) where
+  pure x = WriterT $ pure (x, mempty)
+  (WriterT f) <*> (WriterT a) = undefined -- TODO
