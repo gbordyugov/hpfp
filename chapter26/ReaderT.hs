74e251ed383f0a6fc3db627a09af2aa8225093c0
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 17:46:14 +0200

    Chapter 26 struggling with Applicative instance for ReaderT

--- chapter26/ReaderT.hs
+++ chapter26/ReaderT.hs
@@ -5,6 +5,11 @@ instance (Functor m) => Functor (ReaderT r m) where
   fmap f (ReaderT rma) = ReaderT $ (\r -> fmap f (rma r))
 
 
+{-
+(WriterT f) <*> (WriterT a) = WriterT $ (fmap h f) <*> a where
+  h (f, fw) = \(a, aw) -> (f a, fw `mappend` aw)
+-}
+
 instance (Applicative m) => Applicative (ReaderT r m) where
   pure x = ReaderT $ \r -> pure x
-  (ReaderT rmfa) <*> (ReaderT a) = ReaderT $ undefined
+  (ReaderT f) <*> (ReaderT a) = ReaderT $ undefined
