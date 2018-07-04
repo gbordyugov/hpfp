603a8f8f455d4f568c81fb32f23f0fd25768a24a
Grigory Bordyugov <gregbg@gmail.com>
2018-07-04 11:37:44 +0200

    Chapter 26 applicative instance for Writer

--- chapter26/Writer.hs
+++ chapter26/Writer.hs
@@ -1,4 +1,11 @@
 data Writer w a = Writer { runWriter :: (a, w) }
 
+
 instance Functor (Writer w) where
   fmap f (Writer aw) = Writer $ (f (fst aw), snd aw)
+
+
+instance (Monoid w) => Applicative (Writer w) where
+  pure x = Writer $ (x, mempty)
+  (Writer f) <*> (Writer a) =
+    Writer $ ((fst f) (fst a), (snd f) `mappend` (snd a))
