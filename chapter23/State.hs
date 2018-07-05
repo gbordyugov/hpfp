323b628281ab9b18cfe4d1578f5bcbc0f8180dcc
Grigory Bordyugov <gregbg@gmail.com>
2018-07-05 17:04:25 +0200

    Chapter 23 functor instance for State

--- chapter23/State.hs
+++ chapter23/State.hs
@@ -1,1 +1,5 @@
- data State s a = State { runState :: s -> (a, s) }
+data State s a = State { runState :: s -> (a, s) }
+
+
+instance Functor (State s) where
+  fmap f (State t) = State $ \s -> let (a, s') = t s in (f a, s')
