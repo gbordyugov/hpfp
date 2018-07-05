8eec70d9b316e13bf708497bcd26545aea2a8caa
Grigory Bordyugov <gregbg@gmail.com>
2018-07-05 16:58:17 +0200

    Chapter 26 tinkering applicative instance for StateT

--- chapter26/StateT.hs
+++ chapter26/StateT.hs
@@ -4,3 +4,8 @@ data StateT s m a = StateT { runStateT :: s -> m (a, s) }
 instance (Functor m) => Functor (StateT s m) where
   fmap f (StateT st) = StateT $
     \s -> fmap (\(a, s) -> (f a, s)) (st s)
+
+
+instance (Applicative m) => Applicative (StateT s m) where
+  pure a = StateT $ \s -> pure $ (a, s)
+  (StateT f) <*> (StateT a) = StateT undefined
