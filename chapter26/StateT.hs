0cca7c502dc4f377e20f4c7aeadfc49ea4695cb7
Grigory Bordyugov <gregbg@gmail.com>
2018-07-05 16:40:04 +0200

    Chapter 26 functor instance for StateT

--- chapter26/StateT.hs
+++ chapter26/StateT.hs
@@ -1,1 +1,6 @@
 data StateT s m a = StateT { runStateT :: s -> m (a, s) }
+
+
+instance (Functor m) => Functor (StateT s m) where
+  fmap f (StateT st) = StateT $
+    \s -> fmap (\(a, s) -> (f a, s)) (st s)
