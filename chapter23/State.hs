3605b2d05250d5cd5a7ca728af72ce1f86b7b4a2
Grigory Bordyugov <gregbg@gmail.com>
2018-07-05 17:16:01 +0200

    Chapter 23 applicative instance for State

--- chapter23/State.hs
+++ chapter23/State.hs
@@ -3,3 +3,13 @@ data State s a = State { runState :: s -> (a, s) }
 
 instance Functor (State s) where
   fmap f (State t) = State $ \s -> let (a, s') = t s in (f a, s')
+
+
+instance Applicative (State s) where
+  pure a = State $ \s -> (a, s)
+  (State f) <*> (State a) = State $ \s ->
+    let
+      (h, t) = f s
+      (b, q) = a t
+    in
+      (h b, q)
