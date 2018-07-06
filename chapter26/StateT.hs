a6511d9fcab60cfefea6d33bf5aad328c85e141b
Grigory Bordyugov <gregbg@gmail.com>
2018-07-06 09:19:48 +0200

    Chapter 26 tinkering with applicative instance for StateT

--- chapter26/StateT.hs
+++ chapter26/StateT.hs
@@ -2,10 +2,15 @@ data StateT s m a = StateT { runStateT :: s -> m (a, s) }
 
 
 instance (Functor m) => Functor (StateT s m) where
-  fmap f (StateT st) = StateT $
-    \s -> fmap (\(a, s) -> (f a, s)) (st s)
+  fmap f (StateT st) = StateT $ \s ->
+    fmap (\(a, s) -> (f a, s)) (st s)
 
 
 instance (Applicative m) => Applicative (StateT s m) where
   pure a = StateT $ \s -> pure $ (a, s)
-  (StateT f) <*> (StateT a) = StateT $ \s -> undefined
+  (StateT f) <*> (StateT a) = StateT $ \s ->
+    let
+      fs = f s
+      -- g  = fmap (\(f, s) -> f
+    in
+      undefined
