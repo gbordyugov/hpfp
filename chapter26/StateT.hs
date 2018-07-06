267a871a14dbee5ce2038922e7615ad42a0c1416
Grigory Bordyugov <gregbg@gmail.com>
2018-07-06 12:08:49 +0200

    Chapter 26 one more intermediate research function, a really trivial one

--- chapter26/StateT.hs
+++ chapter26/StateT.hs
@@ -5,13 +5,16 @@ instance (Functor m) => Functor (StateT s m) where
   fmap f (StateT st) = StateT $ \s ->
     fmap (\(a, s) -> (f a, s)) (st s)
 
-tmp :: (s -> (a -> b, s)) -> (s -> (a, s)) -> (s -> (b, s))
-tmp f a = \s ->
+tmp1 :: (s -> (a -> b, s)) -> (s -> (a, s)) -> (s -> (b, s))
+tmp1 f a = \s ->
   let
     (g, t) = f s
     (b, q) = a t
   in
     (g b, q)
+
+tmp2 :: (a -> b, s) -> (a, s) -> (b, s)
+tmp2 (fab, s) (a, t) = (fab a, t)
 
 instance (Applicative m) => Applicative (StateT s m) where
   pure a = StateT $ \s -> pure $ (a, s)
