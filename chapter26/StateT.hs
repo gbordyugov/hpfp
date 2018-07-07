612c67bf9be59f9302fbf10a0ab7d92964a66983
Grigory Bordyugov <gregbg@gmail.com>
2018-07-07 09:59:05 +0200

    Chapter 26 monad instance for StateT

--- chapter26/StateT.hs
+++ chapter26/StateT.hs
@@ -6,61 +6,12 @@ instance (Functor m) => Functor (StateT s m) where
     fmap (\(a, s) -> (f a, s)) (st s)
 
 
-tmp1 :: (s -> (a -> b, s)) -> (s -> (a, s)) -> (s -> (b, s))
-tmp1 f a = \s ->
-  let
-    (g, t) = f s
-    (b, q) = a t
-  in
-    (g b, q)
-
-tmp2 :: (a -> b, s) -> (a, s) -> (b, s)
-tmp2 (fab, s) (a, t) = (fab a, t)
-
-{-
-what :: Applicative m =>
-  StateT s m (a -> b) -> StateT s m a -> StateT s m b
-what (StateT f) (StateT a) = StateT $ \s ->
-  let
-    mfs :: Applicative m => m (a -> b, s)
-    mfs = f s
-    tmp = f' <$> mfs
-    f' :: (a -> b, s) -> (s -> (a, s)) -> (b, s)
-    f' (f'', s) a =
-      let
-        (a', t) = a s
-      in
-        (f'' a', t)
-  in
-    tmp <*> a
--}
-
-{-
-what :: Applicative m => (s -> m (a -> b, s)) -> (s -> m (a, s)) -> (s -> m (b, s))
--}
-what f a = \s ->
-  let
-    mfs = f s
-    tmp = g <$> mfs
-    g (f', s')
-  in
-    undefined
-
+instance (Monad m) => Monad (StateT s m) where
+  return a = StateT $ \s -> return (a, s)
+  (StateT a) >>= f = StateT $ \s -> do
+    (b, t) <- a s
+    (runStateT (f b)) t
 
 instance (Applicative m) => Applicative (StateT s m) where
   pure a = StateT $ \s -> pure $ (a, s)
   (StateT f) <*> (StateT a) = undefined
-  {- (StateT f) <*> (StateT a) = StateT $ \s ->
-    let
-      mfs = f s
-      tmp = h <$> mfs
-      h :: (a -> b, s) -> (s -> (a, s)) -> (b, s)
-      h (f, s) a =
-        let
-          (a', t) = a s
-        in
-          (f a', t)
-    in
-      -- undefined
-      tmp <*> a
-      -}
